5806f1f7934851f7f6d64fdb2f515caae742310d
Yesenia Campos <camposdyes@gmail.com>
2021-02-08 03:55:16 -0600

    Primer Commit

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+
+//objeto global que corre en entorno de node 
+//===========================================
+// Puerto
+//===========================================
+process.env.PORT = process.env.PORT || 3000;
